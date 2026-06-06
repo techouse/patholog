4bf685d8578c782103c1ae5cde3ee0451ee1f1b9
techouse <techouse@users.noreply.github.com>
2026-06-06 11:08:58 +0000

    deploy: 4e7a981183e38b9e7d84b31c7a256b07bbe457fa

---
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["patholog",[["impl ValueEnum for <a class=\"enum\" href=\"patholog/model/enum.PathVariable.html\" title=\"enum patholog::model::PathVariable\">PathVariable</a>",0],["impl ValueEnum for <a class=\"enum\" href=\"patholog/model/enum.PlatformMode.html\" title=\"enum patholog::model::PlatformMode\">PlatformMode</a>",0],["impl ValueEnum for <a class=\"enum\" href=\"patholog/model/enum.PresetKind.html\" title=\"enum patholog::model::PresetKind\">PresetKind</a>",0],["impl ValueEnum for <a class=\"enum\" href=\"patholog/model/enum.ShellKind.html\" title=\"enum patholog::model::ShellKind\">ShellKind</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[607]}
---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["patholog",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"patholog/model/enum.HealthSeverity.html\" title=\"enum patholog::model::HealthSeverity\">HealthSeverity</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"patholog/model/enum.IssueKind.html\" title=\"enum patholog::model::IssueKind\">IssueKind</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[585]}
